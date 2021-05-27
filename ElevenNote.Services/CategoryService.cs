392feb87966885cc6bd78f43317a96698fe557d2
Luis Costilla <luis287@gmail.com>
2021-05-27 15:50:01 -0400

    Put get post and delete methods added in CategoryService

--- ElevenNote.Services/CategoryService.cs
+++ ElevenNote.Services/CategoryService.cs
@@ -33,5 +33,74 @@ namespace ElevenNote.Services
                 return ctx.SaveChanges() == 1;
             }
         }
+
+        //READ(GET)
+        public IEnumerable<CategoryListItem> GetCategory()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Categories
+                        .Where(e => e.OwnerId == _userId)
+                        .Select(
+                            e =>
+                                new CategoryListItem
+                                {
+                                    CategoryId = e.NoteId,
+                                    Name = e.Name,
+                                    
+                                }
+                         );
+                return query.ToArray();
+            }
+        }
+
+        public CategoryDetail GetCategoryById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Categories
+                        .Single(e => e.NoteId == id && e.OwnerId == _userId);
+                return
+                    new CategoryDetail
+                    {
+                        CategoryId = entity.CategoryId,
+                        Name = entity.Name,
+                    };
+            }
+        }
+
+        public bool UpdateCategory(CategoryEdit model)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Categories
+                        .Single(e => e.CategoryId == model.CategoryId && e.OwnerId == _userId);
+
+                entity.Name = model.Name;
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public bool DeleteCategory(int categoryId)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Categories
+                        .Single(e => e.CategoryId == categoryId && e.OwnerId == _userId);
+
+                ctx.Categories.Remove(entity);
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
     }
 }
