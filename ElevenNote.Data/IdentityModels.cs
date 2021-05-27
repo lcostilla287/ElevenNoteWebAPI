3fa7ad7f64e143234499bfaa20aefc6c7bb68668
Luis Costilla <luis287@gmail.com>
2021-05-27 14:49:36 -0400

    category entity added to eleven note data

--- ElevenNote.Data/IdentityModels.cs
+++ ElevenNote.Data/IdentityModels.cs
@@ -35,6 +35,8 @@ namespace ElevenNote.Data
 
         public DbSet<Note> Notes { get; set; }
 
+        public DbSet<Category> Categories { get; set; }
+
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder
