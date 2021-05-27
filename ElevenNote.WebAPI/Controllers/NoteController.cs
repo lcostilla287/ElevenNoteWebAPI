fd38f2095ace0c05d4d1d1e899b2e46396b8de5b
Luis Costilla <luis287@gmail.com>
2021-05-27 13:14:42 -0400

    Note Controller added in web api added create noteservice method and get note service method

---
+++ ElevenNote.WebAPI/Controllers/NoteController.cs
@@ -0,0 +1,30 @@
+﻿using ElevenNote.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ElevenNote.WebAPI.Controllers
+{
+    [Authorize]
+    public class NoteController : ApiController
+    {
+        private NoteService CreateNoteService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var noteservice = new NoteService(userId);
+            return noteservice;
+        }
+
+        //Get All
+        public IHttpActionResult Get()
+        {
+            NoteService noteService = CreateNoteService();
+            var notes = noteService.GetNotes();
+            return Ok(notes);
+        }
+    }
+}
